dcdaabaf8363b95d6c150469a05253c63eb8e31c
konovalov <konovalov@doker.ru>
2023-12-14 16:58:14 +0500

    .

--- InventoryApi/Program.cs
+++ InventoryApi/Program.cs
@@ -1,7 +1,11 @@
 ﻿using ConsoleApp1;
+using InventoryApi;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,16 +23,81 @@ namespace InventoryApi
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Configuration.AddJsonFile("appsettings.json");
+            builder.Configuration.GetConnectionString("TestDB");
+            builder.Build().Run();
+            //Host.CreateDefaultBuilder(args)
+            //   .ConfigureWebHostDefaults(webBuilder =>
+            //   {
+            //       //webBuilder.
+            //       webBuilder.UseStartup<Startup>();
+            //       //webBuilder.UseUrls("http://*:8000");
+            //   }).Build().Run();
+            
+
 
             //Host.CreateDefaultBuilder(args)
             //   .ConfigureWebHostDefaults(webBuilder =>
             //   {
             //       //webBuilder.
             //       webBuilder.UseStartup<Startup>(builder => new Startup());
-            //       webBuilder.UseUrls("http://*:8000");
+            //       //webBuilder.UseUrls("http://*:8000");
             //   }).Build().Run();
-            var app = builder.Build();
-
         }
     }
 }
+
+
+
+//using Microsoft.AspNetCore.Authentication;
+//using Microsoft.AspNetCore.Authentication.Cookies;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using TMS.Operations;
+//using Microsoft.Extensions.Configuration;
+//using TMS.DataAccess;
+//using TMS.Domain;
+//using Microsoft.EntityFrameworkCore;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//builder.Configuration.AddJsonFile("appsettings.json");
+//builder.Services.AddDbContext<ApplicationContext>(options =>
+//        options.UseNpgsql(builder.Configuration.GetConnectionString("TestDB")));
+
+//builder.Services.AddTransient<IClaimsTransformation, ClaimsTransformer>();
+//builder.Services.AddTransient<IRepository, EfCoreRepository>();
+
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//        .AddJwtBearer(options =>
+//        {
+//            options.Authority = "http://tmp.doker.ru:8080/realms/Test2";
+//            options.Audience = "account";
+//            options.RequireHttpsMetadata = false;
+//        });
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
--- InventoryApi/Startup.cs
+++ InventoryApi/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace InventoryApi
 {
@@ -19,13 +19,15 @@ namespace InventoryApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddEndpointsApiExplorer();
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //    .AddJwtBearer(options =>
-            //    {
-            //        options.Authority = "http://tmp.doker.ru:8080/realms/Test2";
-            //        options.Audience = "account";
-            //        options.RequireHttpsMetadata = false;
-            //    });
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "http://tmp.doker.ru:8080/realms/Test2";
+                    options.Audience = "account";
+                    options.RequireHttpsMetadata = false;
+                });
+
             services.AddControllers().AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
